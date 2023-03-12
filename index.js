70b3a5dc0f6c64dc70e338db41a1af46106f041e
Alex Camacho <camacho.ale94@gmail.com>
2023-03-11 19:01:36 -0600

    Create a module to reuse functionality across the project

--- index.js
+++ index.js
@@ -2,22 +2,7 @@ const http = require('http')
 const url = require('url')
 const fs = require('fs')
 
-const replaceTemplate = (template, product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%ID%}/g, product.id)
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-  }
-
-  return output
-}
+const replaceTemplate = require('./modules/replaceTemplate')
 
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template_overview.html`, 'utf-8')
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template_product.html`, 'utf-8')
